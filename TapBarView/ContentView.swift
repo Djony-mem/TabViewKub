4619a62e077fb80c37b2112e3f6140aed8ae992b
Бруйко Евгения <bruyko.e@gmail.com>
2021-02-03 03:00:36 +0300

    Initial Commit

---
+++ TapBarView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TapBarView
+//
+//  Created by Brubrusha on 2/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TapBarView/TapBarViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapBarViewApp.swift
+//  TapBarView
+//
+//  Created by Brubrusha on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TapBarViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
